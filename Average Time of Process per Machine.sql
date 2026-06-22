b0b3dbdcfa42d64a508af211152edefa50b85e2d
azkannawal <azkannawalaf@gmail.com>
2026-06-22 10:44:35 +0700

    feat: add LeetCode 1661

---
+++ Average Time of Process per Machine.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT a.machine_id, ROUND(AVG(ac.timestamp - a.timestamp), 3) AS processing_time
+FROM activity a
+INNER JOIN activity ac
+ON a.process_id = ac.process_id AND a.machine_id = ac.machine_id
+WHERE a.activity_type = 'start' AND ac.activity_type = 'end'
+GROUP BY a.machine_id;
